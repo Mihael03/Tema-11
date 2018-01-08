cb26cc76f39859889912edd3956828f966b597e5
Mihael03 <35213171+Mihael03@users.noreply.github.com>
2018-01-08 08:39:04 +0200

    Add files via upload

---
+++ Lab11_Exercitiul_1.cpp
@@ -0,0 +1,59 @@
+//Dumbrava Mihael
+//Laborator 11
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+
+class Stack
+{
+public:
+	int size() const;
+
+	bool isEmpty() const;
+
+	const T& top() const
+	{
+		if (isEmpty()) throw "Stiva este goala \n";
+		return vect.back();
+	}
+
+	void push(const T& obj);
+
+	void pop();
+
+	vector <T> vect;
+};
+
+template<typename T> bool Stack<T>::isEmpty() const
+{
+	return (vect.empty());
+}
+
+template<typename T> void Stack<T>::push(const T& obj)
+{
+	vect.push_back(obj);
+}
+
+template<typename T> void Stack<T>::pop()
+{
+	if (isEmpty()) throw "Stiva este goala \n";
+	vect.pop_back();
+}
+
+int main() {
+	Stack <int> stiva;
+	cout << "In stiva avem elementele: " << endl;
+	for (int i = 1; i <= 10; i++)
+        stiva.push(i);
+		while (!stiva.isEmpty())
+        {
+			cout << stiva.top() << endl;
+			stiva.pop();
+		}
+
+	return 0;
+}
